167e40cf812fa75afde533c34307257266da6cba
Morteza <mssoftco@yahoo.com>
2022-10-17 15:27:54 +0330

    remove i18n

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: 'build',
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en'
-  },
   ...{ ...(process.env.NODE_ENV === 'production' ? { assetPrefix: '/real-world-front/',images: { unoptimized: true } } : {}) }
 };
 
