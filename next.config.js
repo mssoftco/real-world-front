996a488bb09aaac2b405d34b0cef1b1acf24ff53
Morteza <mssoftco@yahoo.com>
2022-10-17 15:49:05 +0330

    fix path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: 'build',
-  ...{ ...(process.env.NODE_ENV === 'production' ? { assetPrefix: '/real-world-front/',basePath:'/real-world-front/',images: { unoptimized: true } } : {}) }
+  ...{ ...(process.env.NODE_ENV === 'production' ? { assetPrefix: '/real-world-front/',basePath:'/real-world-front',images: { unoptimized: true } } : {}) }
 };
 
 module.exports = nextConfig;
